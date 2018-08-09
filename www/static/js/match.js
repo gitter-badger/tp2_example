6a6cbc2eab9f8e57aceb6ae78e1f70349b48d36d
Gaston Maron <gastonmaron@gmail.com>
2018-08-09 20:54:10 -0300

    Change format ------- Files:  * match.js  * match.html ------- Change the format of the file. Change the timeout to 2 seconds.

--- www/static/js/match.js
+++ www/static/js/match.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){    
-    
-    $.get("/team/" + id_team_west, function( data ) {        
-        $('#logo-'+id_team_west).attr('src', "data:image/png;base64,"+ data.logo);
-        $("#name-"+id_team_west).html(data.name);
+$(document).ready(function() {
+
+    $.get("/team/" + id_team_west, function(data) {
+        $('#logo-' + id_team_west).attr('src', "data:image/png;base64," + data.logo);
+        $("#name-" + id_team_west).html(data.name);
     });
 
-    $.get("/team/" + id_team_east, function( data ) {        
-        $('#logo-'+id_team_east).attr('src', "data:image/png;base64,"+ data.logo);
-        $("#name-"+id_team_east).html(data.name);
+    $.get("/team/" + id_team_east, function(data) {
+        $('#logo-' + id_team_east).attr('src', "data:image/png;base64," + data.logo);
+        $("#name-" + id_team_east).html(data.name);
     });
 
     start_get_score();
@@ -15,28 +15,28 @@ $(document).ready(function(){
 
 var control_score;
 
-function start_get_score(){
-    control_score = setInterval(score, 1000);
+function start_get_score() {
+    control_score = setInterval(score, 2000);
 }
 
-function stop_get_score(){
+function stop_get_score() {
     clearInterval(control_score);
-    stop_chrono();    
+    stop_chrono();
 }
 
-function score(){
-    $.get("/result/match/" + id_match, function( data ) {        
-        $("#score-"+data[0].id_team).html(data[0].score);
-        $("#score-"+data[1].id_team).html(data[1].score);
+function score() {
+    $.get("/result/match/" + id_match, function(data) {
+        $("#score-" + data[0].id_team).html(data[0].score);
+        $("#score-" + data[1].id_team).html(data[1].score);
     });
 }
 
-$("#stop-match").click(function(){
+$("#stop-match").click(function() {
     stop_get_score();
-    $('#stop-match').css('display','none');
-    $('#new-match').css('display','block');
+    $('#stop-match').css('display', 'none');
+    $('#new-match').css('display', 'block');
 
-    $.get("/match/stop/" + id_match, function(data){
+    $.get("/match/stop/" + id_match, function(data) {
         console.log(data);
     }).done(function() {
         score();
